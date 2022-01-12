361f58dd813133df6310191b5806b275503d2bf7
Hongbo Wang <96917934+BOBWang1117@users.noreply.github.com>
2022-01-12 16:39:05 +0800

    Add files via upload

---
+++ code/UGMS_main.java
@@ -0,0 +1,37 @@
+
+public class UGMS_main
+{
+	public static void main(String[] args) {
+		
+		UGMS.databaseName();	//get the database name of the project 
+		do
+		{
+			IoTest.io(UGMS.filename());	//Read the information in the file
+			IoTest.storeIO();
+			
+			
+			String[] a=IoTest.storeIO();
+			StoreRecords.dataFile(a);
+			
+			
+			UGMS.sortingField_1();	//sort the data
+			UGMS.sortingField_2();	//choose the sorting ways
+			
+			UGMS.statisticsReports();
+			//make a statistics report
+			//StoreRecords.cpa();
+		}
+		while (UGMS.judgment().equals("Y"));
+		QueryOnScoreSystem.doing();
+		
+		numOfTxt.AllTxt2();
+		QueryOnScoreSystem.BeginDataStudent();
+		UGMS.SearchGradesByStudent();
+		UGMS.SortingOnGPA();
+		UGMS.SurnameSearching();
+		UGMS.RangingQueryOnGPA();
+		
+	}
+	
+	
+}
